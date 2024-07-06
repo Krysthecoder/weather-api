6b5d0c3767f5fca83c5f007350556adb2a5fd027
The Krys <krysthopher@gmail.com>
2024-07-06 08:57:30 -0600

    updated functionality

--- src/Utils/index.js
+++ src/Utils/index.js
@@ -1,4 +1,4 @@
-import { months } from "./utilsData";
+import { months } from './utilsData';
 
 export const getCurrentMonth = () => {
   const date = new Date();
@@ -14,18 +14,33 @@ export function dateUnixToLocaleConverter(unixVal) {
 export async function fetchWeatherApi(latitude, longitude) {
   const key = process.env.REACT_APP_WEATHER_API_KEY;
   const apiUrl = `${process.env.REACT_APP_WEATHER_API_URL}lat=${latitude}&lon=${longitude}&units=metric&appid=${key}`;
-  const fetchedData = await fetch(apiUrl)
-    .then((response) => response.json())
-    .then((data) => data)
-    .catch((error) => console.log(error));
-  return fetchedData;
+  const fetchedData = await fetch(apiUrl);
+  const fetchedDataResponse = fetchedData.json();
+  return fetchedDataResponse;
 }
 
+const getCurrentLocation = () => {
+  return new Promise((resolve, reject) => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          resolve({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+          });
+        },
+        (error) => {
+          reject(error);
+        }
+      );
+    } else {
+      reject(new Error('unable to get current location.'));
+    }
+  });
+};
+
 export async function fetchDataByGeoLocation() {
-  navigator.geolocation.getCurrentPosition(
-    ({ coords: { latitude, longitude } }) =>
-     fetchWeatherApi(latitude, longitude),
-    () => console.log("unable to get current location")
-  );
-  console.log(navigator)
+  const { latitude, longitude } = await getCurrentLocation();
+  const data = await fetchWeatherApi(latitude, longitude);
+  return data;
 }
--- src/components/FutureWeather.js
+++ src/components/FutureWeather.js
@@ -1,31 +1,55 @@
-import React from "react";
-import { useState, useEffect } from "react";
-import { dateUnixToLocaleConverter, fetchDataByGeoLocation } from "../Utils";
+import React, { useState, useEffect } from 'react';
+import {
+  dateUnixToLocaleConverter,
+  fetchDataByGeoLocation
+  //fetchWeatherApi,
+} from '../Utils';
 
 function FutureWeather() {
-  const [weatherInfo, setWeatherInfo] = useState(null);
+  const [weatherInfo, setWeatherInfo] = useState({});
+  //const [location, setLocation] = useState({ latitude: null, longitude: null });
 
   useEffect(() => {
-    const result = fetchDataByGeoLocation();
-    setWeatherInfo(result);
-    if(result){
-      console.log("useEffectREsult", result);
-    }
+    const test = async () => {
+      const fetchedData = await fetchDataByGeoLocation();
+      setWeatherInfo(fetchedData);
+    };
+    test();
   }, []);
+
+  // useEffect(()=>{
+  //   const getCurrentLocation = () => {
+  //     navigator.geolocation.getCurrentPosition(
+  //       ({ coords: { latitude, longitude } }) => {
+  //         setLocation({latitude, longitude})
+  //       },
+  //       () => console.log("unable to get current location")
+  //     );
+  //   }
+  //   getCurrentLocation()
+  // },[])
+
+  // useEffect(() => {
+  //   if(!!location.latitude && !!location.longitude){
+  //     const fetchApiResolver = async () => {
+  //       const data = await fetchWeatherApi(location.latitude, location.longitude)
+  //       setWeatherInfo(data)
+  //     }
+  //     fetchApiResolver();
+  //   }
+  // },[location])
 
   if (weatherInfo?.daily?.length === 0) {
     return <p>Loading...</p>;
   }
+
   return (
     <div className="w-90 bg-neutral-800 p-4 text-cyan-500">
       <h1>Future predictions:</h1>
       <div className="flex text-center items-center justify-around flex-wrap">
         {weatherInfo?.daily?.map((weatherObj, index) => {
           return (
-            <div
-              key={index}
-              className="flex text-center items-center flex-col w-20  m-4"
-            >
+            <div className="flex text-center items-center flex-col w-20  m-4">
               <img
                 className="w-20 h-20"
                 src={`https://openweathermap.org/img/wn/${weatherObj.weather[0].icon}@2x.png`}
